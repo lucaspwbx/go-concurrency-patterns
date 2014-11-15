f5a3ff2ce6eeb6b405a531b01b583e81c312916b
Lucas Weiblen <lucasweiblen@gmail.com>
2014-11-15 12:57:59 -0200

    add receiving on quit channel

---
+++ receiving_on_quit_channel.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	quit := make(chan string)
+	c := boring("joe", quit)
+	for i := rand.Intn(10); i >= 0; i-- {
+		fmt.Println(<-c)
+	}
+	quit <- "Bye"
+	fmt.Printf("joe says: %q\n", <-quit)
+}
+
+func boring(msg string, quit chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			select {
+			case c <- fmt.Sprintf("%s %d", msg, i):
+			case <-quit:
+				cleanup()
+				quit <- "See you!"
+				return
+			}
+		}
+	}()
+	return c
+}
+
+func cleanup() {
+	fmt.Println("Cleaning up")
+}
